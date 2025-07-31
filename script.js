ee30ac341431685b68b3470f20ef9ea86e4ad8f8
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-07-31 22:34:44 +0200

    Adjustment to clothing brand cards

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const userCardTemplate = document.querySelector("[data-user-template]");
-const userCardContainer = document.querySelector("[data-user-cards-container]");
+const brandCardTemplate = document.querySelector("[data-brand-template]");
+const brandCardContainer = document.querySelector(
+    "[data-brand-cards-container]"
+);
 const searchInput = document.querySelector("[data-search]");
 
-let users = [];
+let brands = [];
 
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase();
-    users.forEach((user) => {
+    brands.forEach((brand) => {
         const isVisible =
-            user.name.toLowerCase().includes(value) ||
-            user.email.toLowerCase().includes(value);
-        user.element.classList.toggle("hide", !isVisible);
+            brand.name.toLowerCase().includes(value) ||
+            brand.category.toLowerCase().includes(value);
+        brand.element.classList.toggle("hide", !isVisible);
     });
 });
 
-fetch("users.json")
+fetch("brands.json")
     .then((res) => res.json())
     .then((data) => {
-        users = data.map((user) => {
-            const card = userCardTemplate.content.cloneNode(true).children[0];
+        brands = data.map((brand) => {
+            const card = brandCardTemplate.content.cloneNode(true).children[0];
             const header = card.querySelector("[data-header]");
             const body = card.querySelector("[data-body]");
-            header.textContent = user.name;
-            body.textContent = user.email;
-            userCardContainer.append(card);
-            return { name: user.name, email: user.email, element: card };
+            header.textContent = brand.name;
+            body.textContent = brand.category;
+            brandCardContainer.append(card);
+            return {
+                name: brand.name,
+                category: brand.category,
+                element: card,
+            };
         });
     });
