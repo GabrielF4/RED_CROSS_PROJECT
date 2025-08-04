965b2bf4309c34b562279abe068e73bac3424873
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-08-04 11:58:01 +0200

    New bransch

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import data from "./brand-generation/clothingBrands.js";
+
+console.log(data);
+
 const brandCardTemplate = document.querySelector("[data-brand-template]");
 const brandCardContainer = document.querySelector(
     "[data-brand-cards-container]"
@@ -5,16 +9,29 @@ const brandCardContainer = document.querySelector(
 const searchInput = document.querySelector("[data-search]");
 const submitButton = document.querySelector("#submit-button");
 
-let brands = [];
-
-submitButton.addEventListener("click", (e) => {
-    const value = searchInput.target.value.toLowerCase();
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
     brands.forEach((brand) => {
         const isVisible = brand.name.toLowerCase().includes(value);
         brand.element.classList.toggle("hide", !isVisible);
     });
 });
 
+const brands = data.map((brand) => {
+    const card = brandCardTemplate.content.cloneNode(true).children[0];
+    const header = card.querySelector("[data-header]");
+    const body = card.querySelector("[data-body]");
+    header.textContent = brand.name;
+    body.textContent = brand.rating;
+    brandCardContainer.append(card);
+    return {
+        name: brand.name,
+        rating: brand.rating,
+        element: card,
+    };
+});
+
+/*
 fetch("brand-generation/clothingBrands.json")
     .then((res) => res.json())
     .then((data) => {
@@ -32,3 +49,4 @@ fetch("brand-generation/clothingBrands.json")
             };
         });
     });
+*/
